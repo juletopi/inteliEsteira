c9d171cc9847d39c129be00bbae62595d2d428a6
Júlio Cézar <juliocezarpvh@hotmail.com>
2026-09-03 21:05:39 -0400

    add service files

---
+++ app/services/status.py
@@ -0,0 +1,9 @@
+def get_system_status():
+    # TODO: substituir dados fixos pelo estado compartilhado do controller.
+    # TODO: incluir produto, QR code, destino e estado detalhado dos componentes.
+    return {
+        "sistema": "IDLE",
+        "arduino": False,
+        "garra": "livre",
+        "esteira": "parada",
+    }
