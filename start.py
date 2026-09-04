d9a5673fba51481c4ee96940a6cb3134c0ac8920
Júlio Cézar <juliocezarpvh@hotmail.com>
2026-09-03 21:04:41 -0400

    add config and start

---
+++ start.py
@@ -0,0 +1,8 @@
+from app import create_app
+from config import FLASK_HOST, FLASK_PORT
+
+app = create_app()
+
+
+if __name__ == "__main__":
+    app.run(host=FLASK_HOST, port=FLASK_PORT, debug=True)
